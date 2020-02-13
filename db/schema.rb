f2ca25d825452efce193b1b69f84cb7469586ab8
syungo kakinuma <momori1995@gmail.com>
2020-02-13 07:03:32 +0000

    create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_094646) do
+ActiveRecord::Schema.define(version: 2020_02_13_070041) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
